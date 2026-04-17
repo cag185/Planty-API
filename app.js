5bb1fc51a8721d28382e95e18014a05a208068b6
cag185 <cag185@pitt.edu>
2026-04-16 20:27:40 -0400

    removed port 8080

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(function (err, req, res, next) {
 });
 
 //set the port number
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
