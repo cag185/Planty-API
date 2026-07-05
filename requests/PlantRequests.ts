3477f30af53863b22bedaf44d665d679eec69514
cag185 <cag185@pitt.edu>
2026-07-05 14:09:06 -0400

    added in support for a plant update field on date last watered

--- requests/PlantRequests.ts
+++ requests/PlantRequests.ts
@@ -10,6 +10,7 @@ export interface UpdatePlantRequest {
   name?: string;
   species?: string;
   watering_frequency_days?: number;
+  date_last_watered?: Date;
 }
 
 export interface DeletePlantRequest {
